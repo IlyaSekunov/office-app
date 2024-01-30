3d82dc35336fbdb4b39839b995e34ad5c4c34a02
Sekunov Ilya <sekunovila@gmail.com>
2024-01-30 13:19:56 +0300

    user view model add

--- app/src/main/java/ru/ilyasekunov/officeapp/ui/userprofile/UserViewModel.kt
+++ app/src/main/java/ru/ilyasekunov/officeapp/ui/userprofile/UserViewModel.kt
@@ -4,29 +4,51 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import ru.ilyasekunov.officeapp.preview.officeListPreview
-import ru.ilyasekunov.officeapp.preview.userInfoUiStatePreview
-import ru.ilyasekunov.officeapp.ui.components.Office
+import kotlinx.coroutines.launch
+import ru.ilyasekunov.officeapp.data.model.Office
+import ru.ilyasekunov.officeapp.data.model.User
+import ru.ilyasekunov.officeapp.data.officeList
+import ru.ilyasekunov.officeapp.data.repository.user.UserRepository
 import javax.inject.Inject
 
+fun User.toUserInfoUiState(): UserInfoUiState =
+    UserInfoUiState(
+        email = email,
+        password = password,
+        name = name,
+        surname = surname,
+        job = job,
+        photo = photo,
+        office = office
+    )
+
 data class UserInfoUiState(
+    val email: String = "",
+    val password: String = "",
+    val repeatedPassword: String = "",
     val name: String = "",
     val surname: String = "",
     val job: String = "",
     val photo: String? = null,
-    val office: Office = Office(0, "", "")
+    val office: Office = officeList[0]
 )
 
 @HiltViewModel
-class UserInfoViewModel @Inject constructor() : ViewModel() {
+class UserViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
+    var user by mutableStateOf<User?>(null)
+        private set
     var userInfoUiState by mutableStateOf(UserInfoUiState())
         private set
-
-    val officeList = officeListPreview
+    var isUserFetching by mutableStateOf(false)
+        private set
+    val officeList = userRepository.findOfficeList()
 
     init {
-        userInfoUiState = userInfoUiStatePreview
+        fetchUserInfo()
     }
 
     fun updateName(name: String) {
@@ -50,10 +72,25 @@ class UserInfoViewModel @Inject constructor() : ViewModel() {
     }
 
     fun save() {
-
+        user = user?.copy(
+            name = userInfoUiState.name,
+            surname = userInfoUiState.surname,
+            job = userInfoUiState.job,
+            photo = userInfoUiState.photo,
+            office = userInfoUiState.office
+        )
     }
 
     fun logout() {
 
+    }
+
+    private fun fetchUserInfo() {
+        viewModelScope.launch {
+            isUserFetching = true
+            user = userRepository.findUser()
+            userInfoUiState = user!!.toUserInfoUiState()
+            isUserFetching = false
+        }
     }
 }
