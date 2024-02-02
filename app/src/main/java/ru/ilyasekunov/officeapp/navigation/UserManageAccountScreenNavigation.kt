77fad417ab41f8d391212a01542f2435fdfd6512
Sekunov Ilya <sekunovila@gmail.com>
2024-02-02 18:47:59 +0300

    view model scoping add, converters add gallery launcher in coroutine scope add

--- app/src/main/java/ru/ilyasekunov/officeapp/navigation/UserManageAccountScreenNavigation.kt
+++ app/src/main/java/ru/ilyasekunov/officeapp/navigation/UserManageAccountScreenNavigation.kt
@@ -1,16 +1,23 @@
 package ru.ilyasekunov.officeapp.navigation
 
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import ru.ilyasekunov.officeapp.ui.userprofile.UserManageAccountScreen
 import ru.ilyasekunov.officeapp.ui.userprofile.UserViewModel
+import ru.ilyasekunov.officeapp.util.toBitmap
+import ru.ilyasekunov.officeapp.util.toByteArray
 
 fun NavGraphBuilder.userManageAccountScreen(
     viewModelStoreOwnerProvider: () -> ViewModelStoreOwner,
@@ -23,27 +30,53 @@ fun NavGraphBuilder.userManageAccountScreen(
     composable(route = Screen.UserManageAccount.route) { backStackEntry ->
         val viewModelStoreOwner = remember(backStackEntry) { viewModelStoreOwnerProvider() }
         val userInfoViewModel = hiltViewModel<UserViewModel>(viewModelStoreOwner)
+        val contentResolver = LocalContext.current.contentResolver
+        val coroutineScope = rememberCoroutineScope { Dispatchers.IO }
         val galleryLauncher =
-            rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) {
-                userInfoViewModel.updatePhotoUri(it.toString())
+            rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) {
+                coroutineScope.launch {
+                    val bitmap = it.toBitmap(contentResolver)
+                    val photo = bitmap.toByteArray()
+                    userInfoViewModel.updatePhoto(photo)
+                }
             }
         UserManageAccountScreen(
             userInfoUiState = userInfoViewModel.userInfoUiState,
             officeList = userInfoViewModel.officeList,
-            onPhotoPickerClick = { galleryLauncher.launch("image/*") },
+            onPhotoPickerClick = {
+                galleryLauncher.launch(
+                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                )
+            },
             onNameValueChange = userInfoViewModel::updateName,
             onSurnameValueChange = userInfoViewModel::updateSurname,
             onJobValueChange = userInfoViewModel::updateJob,
             onOfficeChange = userInfoViewModel::updateOffice,
+            isSaveButtonEnabled = userInfoViewModel.isUserNewInfoUnsaved,
             onSaveButtonClick = {
                 userInfoViewModel.save()
                 navigateToProfileScreen()
             },
-            navigateToHomeScreen = navigateToHomeScreen,
-            navigateToFavouriteScreen = navigateToFavouriteScreen,
-            navigateToMyOfficeScreen = navigateToMyOfficeScreen,
-            navigateToProfileScreen = navigateToProfileScreen,
-            navigateBack = navigateBack
+            navigateToHomeScreen = {
+                userInfoViewModel.restoreUserInfoUiChanges()
+                navigateToHomeScreen()
+            },
+            navigateToFavouriteScreen = {
+                userInfoViewModel.restoreUserInfoUiChanges()
+                navigateToFavouriteScreen()
+            },
+            navigateToMyOfficeScreen = {
+                userInfoViewModel.restoreUserInfoUiChanges()
+                navigateToMyOfficeScreen()
+            },
+            navigateToProfileScreen = {
+                userInfoViewModel.restoreUserInfoUiChanges()
+                navigateToProfileScreen()
+            },
+            navigateBack = {
+                userInfoViewModel.restoreUserInfoUiChanges()
+                navigateBack()
+            }
         )
     }
 }
--- app/src/main/java/ru/ilyasekunov/officeapp/navigation/auth/registration/RegistrationUserInfoScreenNavigation.kt
+++ app/src/main/java/ru/ilyasekunov/officeapp/navigation/auth/registration/RegistrationUserInfoScreenNavigation.kt
@@ -1,18 +1,25 @@
 package ru.ilyasekunov.officeapp.navigation.auth.registration
 
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import ru.ilyasekunov.officeapp.data.officeList
 import ru.ilyasekunov.officeapp.navigation.Screen
 import ru.ilyasekunov.officeapp.ui.auth.registration.RegistrationUserInfoScreen
 import ru.ilyasekunov.officeapp.ui.auth.registration.RegistrationViewModel
+import ru.ilyasekunov.officeapp.util.toBitmap
+import ru.ilyasekunov.officeapp.util.toByteArray
 
 fun NavGraphBuilder.registrationUserInfoScreen(
     viewModelStoreOwnerProvider: () -> ViewModelStoreOwner,
@@ -20,20 +27,27 @@ fun NavGraphBuilder.registrationUserInfoScreen(
     navigateToMainGraph: () -> Unit
 ) {
     composable(Screen.RegistrationUserInfo.route) { backStackEntry ->
-        val viewModelStoreOwner = remember(backStackEntry) {
-            viewModelStoreOwnerProvider()
-        }
-        val registrationViewModel =
-            hiltViewModel<RegistrationViewModel>(viewModelStoreOwner)
+        val viewModelStoreOwner = remember(backStackEntry) { viewModelStoreOwnerProvider() }
+        val registrationViewModel = hiltViewModel<RegistrationViewModel>(viewModelStoreOwner)
+        val contentResolver = LocalContext.current.contentResolver
+        val coroutineScope = rememberCoroutineScope { Dispatchers.IO }
         val galleryLauncher =
-            rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) {
-                registrationViewModel.updatePhoto(it.toString())
+            rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) {
+                coroutineScope.launch {
+                    val bitmap = it.toBitmap(contentResolver)
+                    val photo = bitmap.toByteArray()
+                    registrationViewModel.updatePhoto(photo)
+                }
             }
         RegistrationUserInfoScreen(
             userInfoUiState = registrationViewModel.registrationUiState.userInfoUiState,
             officeList = officeList,
             navigateBack = navigateBack,
-            onPhotoPickerClick = { galleryLauncher.launch("image/*") },
+            onPhotoPickerClick = {
+                galleryLauncher.launch(
+                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                )
+            },
             onNameValueChange = registrationViewModel::updateName,
             onSurnameValueChange = registrationViewModel::updateSurname,
             onJobValueChange = registrationViewModel::updateJob,
