60fb775c45a6bbe675f2c68c3395189cfd6ec110
Sekunov Ilya <sekunovila@gmail.com>
2024-02-15 22:51:42 +0300

    home screen post text add text alignment justify

--- app/src/main/java/ru/ilyasekunov/officeapp/ui/home/HomeScreen.kt
+++ app/src/main/java/ru/ilyasekunov/officeapp/ui/home/HomeScreen.kt
@@ -66,6 +66,7 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpSize
@@ -345,6 +346,7 @@ fun IdeaPost(
                 style = MaterialTheme.typography.bodyMedium,
                 fontSize = 14.sp,
                 overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Justify,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(start = 16.dp, end = 16.dp)
