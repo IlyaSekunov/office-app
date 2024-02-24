3fdcab1122e64fc02fe489a855fa0d02a4ccff14
Sekunov Ilya <sekunovila@gmail.com>
2024-02-24 11:27:24 +0300

    add auth api

--- app/src/main/java/ru/ilyasekunov/officeapp/data/api/AuthApi.kt
+++ app/src/main/java/ru/ilyasekunov/officeapp/data/api/AuthApi.kt
@@ -1,4 +1,4 @@
-package ru.ilyasekunov.officeapp.data.datasource
+package ru.ilyasekunov.officeapp.data.api
 
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -6,11 +6,13 @@ import ru.ilyasekunov.officeapp.data.dto.LoginForm
 import ru.ilyasekunov.officeapp.data.dto.RegistrationForm
 import ru.ilyasekunov.officeapp.data.model.User
 
-interface AuthDatasource {
+interface AuthApi {
     @POST("auth/register")
     suspend fun register(registrationForm: RegistrationForm): String
     @GET("auth/user-info")
-    suspend fun userInfo(token: String): User
+    suspend fun userInfo(): User
     @GET("auth/login")
     suspend fun login(loginForm: LoginForm): String
+    @GET("auth/update-token")
+    suspend fun updateToken(): String
 }
