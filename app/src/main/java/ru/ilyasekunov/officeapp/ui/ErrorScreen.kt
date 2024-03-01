e6513b2230d91bbb2dd20cd1b3db2d0399744a73
Sekunov Ilya <sekunovila@gmail.com>
2024-03-01 19:32:43 +0300

    error screen retry button on press animation add

--- app/src/main/java/ru/ilyasekunov/officeapp/ui/ErrorScreen.kt
+++ app/src/main/java/ru/ilyasekunov/officeapp/ui/ErrorScreen.kt
@@ -1,18 +1,36 @@
 package ru.ilyasekunov.officeapp.ui
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import ru.ilyasekunov.officeapp.ui.auth.registration.RetryButton
+import ru.ilyasekunov.officeapp.R
 
 @Composable
 fun ErrorScreen(
@@ -33,6 +51,49 @@ fun ErrorScreen(
         Spacer(modifier = Modifier.height(30.dp))
         RetryButton(
             onClick = onRetryButtonClick
+        )
+    }
+}
+
+@Composable
+fun RetryButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val animatedScale by animateFloatAsState(
+        targetValue = if (isPressed) 0.9f else 1f,
+        animationSpec = tween(durationMillis = 100),
+        label = "animated_scale"
+    )
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .selectable(
+                selected = isPressed,
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = onClick
+            )
+            .scale(animatedScale)
+            .clip(MaterialTheme.shapes.medium)
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(horizontal = 10.dp, vertical = 5.dp)
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.baseline_refresh_24),
+            contentDescription = "refresh_button",
+            tint = MaterialTheme.colorScheme.onPrimary,
+            modifier = Modifier.size(20.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(
+            text = stringResource(R.string.retry),
+            fontSize = 14.sp,
+            style = MaterialTheme.typography.bodyLarge,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onPrimary
         )
     }
 }
--- app/src/main/java/ru/ilyasekunov/officeapp/ui/auth/registration/RegistrationMainScreen.kt
+++ app/src/main/java/ru/ilyasekunov/officeapp/ui/auth/registration/RegistrationMainScreen.kt
@@ -10,23 +10,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -134,36 +129,6 @@ fun LoginSection(
             fontSize = 14.sp,
             color = MaterialTheme.colorScheme.primary,
             modifier = Modifier.clickable { onLoginClick() }
-        )
-    }
-}
-
-@Composable
-fun RetryButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .clip(MaterialTheme.shapes.medium)
-            .background(MaterialTheme.colorScheme.primary)
-            .clickable { onClick() }
-            .padding(horizontal = 10.dp, vertical = 5.dp)
-    ) {
-        Icon(
-            painter = painterResource(R.drawable.baseline_refresh_24),
-            contentDescription = "refresh_button",
-            tint = MaterialTheme.colorScheme.onPrimary,
-            modifier = Modifier.size(20.dp)
-        )
-        Spacer(modifier = Modifier.width(10.dp))
-        Text(
-            text = stringResource(R.string.retry),
-            fontSize = 14.sp,
-            style = MaterialTheme.typography.bodyLarge,
-            textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.onPrimary
         )
     }
 }
