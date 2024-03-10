f3b45c374b9085d0d6651ccaa468edafa60936e6
Sekunov Ilya <sekunovila@gmail.com>
2024-03-10 13:42:53 +0300

    mock auth data source add

---
+++ app/src/main/java/ru/ilyasekunov/officeapp/data/datasource/local/mock/MockAuthDataSource.kt
@@ -0,0 +1,28 @@
+package ru.ilyasekunov.officeapp.data.datasource.local.mock
+
+import ru.ilyasekunov.officeapp.data.datasource.AuthDataSource
+import ru.ilyasekunov.officeapp.data.dto.LoginForm
+import ru.ilyasekunov.officeapp.data.dto.RegistrationForm
+import ru.ilyasekunov.officeapp.data.model.Tokens
+import ru.ilyasekunov.officeapp.data.model.User
+
+class MockAuthDataSource : AuthDataSource {
+    override suspend fun register(registrationForm: RegistrationForm): Result<Tokens> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun userInfo(): Result<User> =
+        if (User != null) Result.success(User!!) else Result.failure(Exception())
+
+    override suspend fun login(loginForm: LoginForm): Result<Tokens> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun refreshToken(refreshToken: String): Result<Tokens> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun logout(): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+}
