2d1114351b9e1127681c448c1f8ac6f30651d63a
Sekunov Ilya <sekunovila@gmail.com>
2024-03-17 15:53:55 +0300

    add @Body annotation to user api method

--- app/src/main/java/ru/ilyasekunov/officeapp/data/api/UserApi.kt
+++ app/src/main/java/ru/ilyasekunov/officeapp/data/api/UserApi.kt
@@ -1,12 +1,11 @@
 package ru.ilyasekunov.officeapp.data.api
 
 import retrofit2.Response
-import retrofit2.http.GET
+import retrofit2.http.Body
 import retrofit2.http.PATCH
 import ru.ilyasekunov.officeapp.data.dto.UserDto
-import ru.ilyasekunov.officeapp.data.model.Office
 
 interface UserApi {
     @PATCH("users")
-    suspend fun saveChanges(userDto: UserDto): Response<Unit>
+    suspend fun saveChanges(@Body userDto: UserDto): Response<Unit>
 }
