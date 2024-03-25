bf9f490d7118672808f4499657b1f073842e2f22
Sekunov Ilya <sekunovila@gmail.com>
2024-03-25 15:35:18 +0300

    delete serializable name

--- app/src/main/java/ru/ilyasekunov/officeapp/data/model/Filters.kt
+++ app/src/main/java/ru/ilyasekunov/officeapp/data/model/Filters.kt
@@ -1,10 +1,7 @@
 package ru.ilyasekunov.officeapp.data.model
 
-import com.google.gson.annotations.SerializedName
-
 data class Filters(
     val offices: List<Office>,
-    @SerializedName("sortingFilters")
     val sortingCategories: List<SortingCategory>
 )
 
