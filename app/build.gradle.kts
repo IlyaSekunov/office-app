ba09b4e5656992f6410b35fed4611e4ea9ac1e5a
Sekunov Ilya <sekunovila@gmail.com>
2024-03-29 16:43:54 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,13 +57,13 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
+    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
 
     // Paging library
-    implementation("androidx.paging:paging-compose:3.3.0-alpha04")
+    implementation("androidx.paging:paging-compose:3.3.0-alpha05")
 
     // Shimmer effect
     implementation("com.valentinilk.shimmer:compose-shimmer:1.2.0")
@@ -102,7 +102,7 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
