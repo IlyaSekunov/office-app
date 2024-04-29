52d880d0d8c87e97ff9d6426d0677601854b28fb
IlyaSekunov <sekunovila@gmail.com>
2024-04-29 11:46:59 +0300

    Delete commented code in PagingDataSource.kt

--- app/src/main/java/ru/ilyasekunov/officeapp/data/datasource/pager/PagingDataSource.kt
+++ app/src/main/java/ru/ilyasekunov/officeapp/data/datasource/pager/PagingDataSource.kt
@@ -27,32 +27,4 @@ class PagingDataSource<DataSource, Model : Any>(
         }
         return LoadResult.Error(requestResult.exceptionOrNull()!!)
     }
-
 }
-
-/*
-class AuthorsPagingDataSource(
-    private val authorDataSource: AuthorDataSource,
-    private val request: suspend AuthorDataSource.(page: Int) -> Result<List<IdeaAuthor>>
-) : PagingSource<Int, IdeaAuthor>() {
-    override fun getRefreshKey(state: PagingState<Int, IdeaAuthor>): Int? {
-        return state.anchorPosition?.let { anchorPosition ->
-            val anchorPage = state.closestPageToPosition(anchorPosition)
-            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
-        }
-    }
-
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, IdeaAuthor> {
-        val page = params.key ?: 1
-        val authorsResult = authorDataSource.request(page)
-        if (authorsResult.isSuccess) {
-            val authors = authorsResult.getOrThrow()
-            return LoadResult.Page(
-                data = authors,
-                prevKey = if (page == 1) null else page - 1,
-                nextKey = if (authors.isEmpty()) null else page + 1
-            )
-        }
-        return LoadResult.Error(authorsResult.exceptionOrNull()!!)
-    }
-}*/
