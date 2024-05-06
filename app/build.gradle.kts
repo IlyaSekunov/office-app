2d4b5a61d5868539acca384bf216cb4c00084e7f
Sekunov Ilya <sekunovila@gmail.com>
2024-05-06 19:34:51 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,23 +54,23 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
-    implementation("androidx.compose.ui:ui:1.7.0-alpha07")
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    implementation("androidx.compose.ui:ui:1.7.0-alpha08")
 
     // Lottie
     implementation("com.airbnb.android:lottie-compose:6.4.0")
 
     // Paging library
-    implementation("androidx.paging:paging-compose:3.3.0-beta01")
+    implementation("androidx.paging:paging-compose:3.3.0-rc01")
 
     // Shimmer effect
     implementation("com.valentinilk.shimmer:compose-shimmer:1.2.0")
 
     // DataStore
-    implementation("androidx.datastore:datastore-preferences:1.1.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     // Material
     implementation("androidx.compose.material3:material3:1.2.1")
@@ -100,7 +100,7 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
