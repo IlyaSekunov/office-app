fe0f3d0a9ad8b4e0008f562e4c2e0a11a7f92a8a
Sekunov Ilya <sekunovila@gmail.com>
2024-05-20 22:21:37 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,16 +55,16 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
-    implementation("androidx.compose.ui:ui:1.7.0-alpha08")
+    implementation("androidx.compose.ui:ui:1.7.0-beta01")
 
     // Lottie
     implementation("com.airbnb.android:lottie-compose:6.4.0")
 
     // Paging library
-    implementation("androidx.paging:paging-compose:3.3.0-rc01")
+    implementation("androidx.paging:paging-compose:3.3.0")
 
     // Shimmer effect
     implementation("com.valentinilk.shimmer:compose-shimmer:1.2.0")
